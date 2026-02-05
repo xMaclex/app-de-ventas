732d97b7ba018e82f2d7cd1bb316baedfe36ccd8
Michael Sanchez <Michael.s140701@gmail.com>
2026-02-05 15:18:24 -0400

    Correcciones

--- ventaapp/obj/Debug/net9.0/ventaapp.AssemblyInfo.cs
+++ ventaapp/obj/Debug/net9.0/ventaapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ventaapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8025a00f254c2b394aece4000b0ba160f38467db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d60c23dcddfd3022fe40a17f49c3e74cb8910a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ventaapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ventaapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
