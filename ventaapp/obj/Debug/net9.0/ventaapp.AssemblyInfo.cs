98ffdda8ef63d01d05e5c4f2290114e5870da627
Michael Sanchez <Michael.s140701@gmail.com>
2026-02-12 08:40:58 -0400

    .

--- ventaapp/obj/Debug/net9.0/ventaapp.AssemblyInfo.cs
+++ ventaapp/obj/Debug/net9.0/ventaapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ventaapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42c32d3fee85aeaf436da5f475c5fab4024e911b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f367143f808e574a6cf511f3cf90f14ecbb5cc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ventaapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ventaapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
